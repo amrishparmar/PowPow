100f35321328142b17875f27a4659da12aa00d20
Amrish Parmar <aparm001@gold.ac.uk>
2016-02-11 15:50:16 +0000

    added basic code for bullets

--- scripts/Game.js
+++ scripts/Game.js
@@ -25,6 +25,81 @@ PowPow.Game = function (game) {
 
 };
 
+var Bullet = function(game, key) {
+    Phaser.Sprite.call(this, game, 0, 0, key);
+    this.texture.baseTexture.scaleMode = PIXI.scaleModes.NEAREST;
+
+    this.anchor.set(0.5);
+
+    this.checkWorldBounds = true;
+    this.outOfBoundsKill = true;
+    this.exists = false;
+
+    this.tracking = false;
+    this.scaleSpeed = 0;
+};
+
+Bullet.prototype = Object.create(Phaser.Sprite.prototype);
+Bullet.prototype.constructor = Bullet;
+
+Bullet.prototype.fire = function(x, y, angle, speed, gx, gy) {
+    gx = gx || 0;
+    gy = gy || 0;
+
+    this.reset(x, y);
+    this.scale.set(1);
+
+    this.game.physics.arcade.velocityFromAngle(angle, speed, this.body.velocity);
+
+    this.angle = angle;
+
+    this.body.gravity.set(gx, gy);
+};
+
+Bullet.prototype.update = function() {
+    if (this.tracking) {
+        this.rotation = Math.atan2(this.body.velocity.y, this.body.velocity.x);
+    }
+
+    if (this.scaleSpeed > 0) {
+        this.scale.x += this.scaleSpeed;
+        this.scale.y += this.scaleSpeed;
+    }
+};
+
+var Weapon = {};
+
+//////// Auto handgun ////////
+
+Weapon.AutoHG = function(game) {
+    Phaser.Group.call(this, game, game.world, 'Handgun', false, true, Phaser.Physics.ARCADE);
+
+    this.nextFire = 0;
+    this.bulletSpeed = 600;
+    this.fireRate = 100;
+
+    for (var i = 0; i < 16; i++) {
+        this.add(new Bullet(game, 'bullet'), true);
+    }
+
+    return this;
+};
+
+Weapon.AutoHG.prototype = Object.create(Phaser.Group.prototype);
+Weapon.AutoHG.prototype.constructor = Weapon.AutoHG;
+
+Weapon.AutoHG.prototype.fire = function(source) {
+    if (this.game.time.time < this.nextFire) {
+        return;
+    }
+    var x = source.x + 10;
+    var y = source.y + 10;
+
+    this.getFirstExists(false).fire(x, y, 0, this.bulletSpeed, 0, 0);
+
+    this.nextFire = this.game.time.time + this.fireRate;
+};
+
 var player;
 var platforms;
 
@@ -36,8 +111,13 @@ PowPow.Game.prototype = {
     preload: function() {
         this.load.image('city', 'images/maps/city/city_bg.jpg');
         this.load.image('ground', 'images/maps/city/city_platform.png');
-        // this.load.image('player', 'images/player.png');
-        this.load.spritesheet('player', 'images/sprite/soldier.png', 175, 200, 10);
+        this.load.image('player', 'images/player.png');
+        //this.load.spritesheet('player', 'images/sprite/soldier.png', 175, 200, 10);
+        this.load.image('bullet', 'images/bullet.png');
+
+        this.game.scale.pageAlignHorizontally = true;
+        this.game.scale.pageAlignVertically = true;
+        this.game.scale.refresh();
     },
 
     create: function () {
@@ -87,7 +167,7 @@ PowPow.Game.prototype = {
         player.body.collideWorldBounds = true;
         
         // player animation frames
-        player.animations.add('left', [0, 1, 2, 3, 4], 20, true);
+        //player.animations.add('left', [0, 1, 2, 3, 4], 20, true);
         
         // create an object which stores the input keys for movement 
         keys = this.input.keyboard.addKeys({ 
