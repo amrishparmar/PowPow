6355198a0b015cbf925dca197531a99fecef7711
amrishparmar <amrish.parmar@me.com>
2016-02-11 11:41:58 +0000

    renamed city bg file and added comments to game.js

--- scripts/Game.js
+++ scripts/Game.js
@@ -34,15 +34,18 @@ var keys;
 PowPow.Game.prototype = {
 
     preload: function() {
-        this.load.image('city', 'images/maps/city/city.jpg');
+        this.load.image('city', 'images/maps/city/city_bg.jpg');
         this.load.image('ground', 'images/maps/city/city_platform.png');
         // this.load.image('player', 'images/player.png');
         this.load.spritesheet('player', 'images/sprite/soldier.png', 175, 200, 10);
     },
 
     create: function () {
+        // add the city background image and set the world bounds to same dimensions
         this.add.tileSprite(0, 0, 1600, 1000, 'city');
         this.world.setBounds(0, 0, 1600, 1000);
+        
+        // using arcade physics for now
         this.physics.startSystem(Phaser.Physics.ARCADE);
         
         platforms = this.add.group();
@@ -51,6 +54,8 @@ PowPow.Game.prototype = {
         ground.scale.setTo(4.5, 2);
         ground.body.immovable = true;
         
+        
+        // create a bunch of platforms based on the game design docs
         var ledge = platforms.create(-150, 400, 'ground');
         ledge.body.immovable = true;
     
@@ -72,42 +77,50 @@ PowPow.Game.prototype = {
         ledge = platforms.create(1500, 300, 'ground');
         ledge.body.immovable = true;
         
+        // add the player to the world
         player = this.add.sprite(32, this.world.height - 300, 'player');
         this.physics.arcade.enable(player);
         
+        // enable gravity and collision with side of screen
         player.body.bounce.y = 0.2;
         player.body.gravity.y = 600;
         player.body.collideWorldBounds = true;
         
+        // player animation frames
         player.animations.add('left', [0, 1, 2, 3, 4], 20, true);
         
+        // create an object which stores the input keys for movement 
         keys = this.input.keyboard.addKeys({ 
-            'up': Phaser.KeyCode.W, 'down': Phaser.KeyCode.S, 'left': Phaser.KeyCode.A, 'right': Phaser.KeyCode.D 
+            'up': Phaser.KeyCode.W, 'left': Phaser.KeyCode.A, 'right': Phaser.KeyCode.D 
         });
         
+        // allow the camera to follow and be centered on the player
         this.camera.follow(player);
-
     },
 
     update: function () {
+        // enable/check collision between the player and the platforms
         this.physics.arcade.collide(player, platforms);
         
+        // when a key is not pressed don't want the player to move
         player.body.velocity.x = 0;
         
+        // move the player left if a key is pressed
         if (keys.left.isDown) {
             player.body.velocity.x = -300;
             player.animations.play('left');
         }
+        // move the player right if d key is pressed
         else if (keys.right.isDown) {
             player.body.velocity.x = 300;
         }
+        // otherwise stop the animation and display the standing frame
         else {
-            // player.animations.stop();
             player.animations.stop();
-
             player.frame = 9;
         }
         
+        // allow the player to jump if they press the w key and the are currently standing on a platform  
         if (keys.up.isDown && player.body.touching.down) {
             player.body.velocity.y = -600;
         }
